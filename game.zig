681210d402ee0ebc80784922d8a7cd6555dcb827
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-07-23 07:50:10 -0500

    2nd

--- game.zig
+++ game.zig
@@ -3,9 +3,20 @@
 //ReleaseSafe has runtime safety, Debug is default also has runtime safety.
 //Currently for x86_64-windows, this produces a 2.5KiB executable.
 const std = @import("std");
+const Input = @import("input.zig");
 
-pub fn main() void {
-    std.io.getStdOut().writeAll(
-        "Hello World!",
-    ) catch unreachable;
+pub fn main() !void {
+    var allocator = std.heap.page_allocator;
+
+    // Print a prompt to the console
+    try std.io.getStdOut().writer().print("Enter something: ", .{});
+
+    // Prepare a buffer for the input
+    var buffer: [512]u8 = undefined;
+
+    // Read input from the user
+    const line = try std.io.getStdIn().reader().readUntilDelimiterOrEof(&allocator, '\n', buffer[0..]);
+
+    // Print the input back to the console
+    try std.io.getStdOut().writer().print("You entered: {}\n", .{line});
 }
